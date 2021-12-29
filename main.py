e01a909d838b60e4ca2db73d5f208500b7a82829
fcmdhyd <69862604+fcmdhyd@users.noreply.github.com>
2021-12-29 08:49:57 +0000

    Made code more readable with space.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import art
 print(art.logo)
 
 def find_highest_bidder(bidding_record):
+  
     highest_bid = 0
     winner = ""
     # bidding_record = {"Angela": 123, "James"： 321}
     for bidder in bidding_record:
       bid_amount = bidding_record[bidder]
+
       if bid_amount > highest_bid:
         highest_bid = bid_amount
         winner = bidder
+
     print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 bidding_finished = False
@@ -29,6 +32,9 @@ while not bidding_finished:
     find_highest_bidder(bids)
   elif should_continue == "yes":
     clear()
+
+
+
 
   
 
